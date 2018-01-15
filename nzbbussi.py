3a217612026ba9c2f10b7ee274b728e46d49bf52
dermatty <stephan@untergrabner.at>
2018-01-15 21:29:51 +0100

    config & yenc analysis 1

--- nzbbussi.py
+++ nzbbussi.py
@@ -3,25 +3,30 @@ import xml.etree.ElementTree as ET
 import time
 import yenc
 from threading import Thread
-import sys
+import glob
 import ssl
 import posix_ipc
 import configparser
 import subprocess
 import os
+from os.path import expanduser
+import sys
+
+USERHOME = expanduser("~")
+MAIN_DIR = USERHOME + "/.nzbbussi/"
+CONFIG_DIR = MAIN_DIR + "config/"
+NZB_DIR = MAIN_DIR + "nzb/"
+COMPLETE_DIR = MAIN_DIR + "complete/"
+INCOMPLETE_DIR = MAIN_DIR + "incomplete/"
+LOGS_DIR = MAIN_DIR + "logs/"
 
 # python 3 unrar
 # https://pypi.python.org/pypi/unrar/
 # https://github.com/markokr/rarfile
 
-# NZB = "saltatio-mortis.nzb"
-# NZB = "Vorstadtweiber.nzb"
-# NZB = "Fuenf.Freunde.1.nzb"
-NZB = "nzb/Der.Gloeckner.von.Notre.nzb"
 
 SEMAPHORE = posix_ipc.Semaphore("news_sema", posix_ipc.O_CREAT)
 SEMAPHORE.release()
-# SEMAPHORE.acquire
 
 BYTES_WRITTEN = 0
 DTIME = 0
@@ -56,22 +61,8 @@ class Download(Thread):
 
     def run(self):
         global BYTES_WRITTEN
+        global COMPLETE_DIR
         for header, filelist in self.ITEMS:
-            # nr_articles = len(filelist)
-            '''byt = bytearray()
-            nr = 1
-            for f in filelist:
-                if nr > int(filelist[0]):
-                    break
-                if f == filelist[0]:
-                    continue
-                print(str(self.THREADNO) + " > File:" + header + " --> downloading article #" + str(f[1]) + " of " + filelist[0] + ": " + f[0])
-                resp, info = self.SERVER.body(f[0])
-                del info.lines[-1]
-                del info.lines[0:2]
-                for inf in info.lines:
-                    byt.extend(inf)
-                nr += 1'''
             nr_articles = int(filelist[0])
             articlesprocessed = []
             byt = bytearray()
@@ -81,6 +72,43 @@ class Download(Thread):
                 if f[1] not in articlesprocessed:
                     print(str(self.THREADNO) + " > File:" + header + " --> downloading article #" + str(f[1]) + " of " + str(nr_articles) + ": " + f[0])
                     resp, info = self.SERVER.body(f[0])
+                    resp_h, info_h = self.SERVER.body(f[0])
+
+                    # todo: if 2 articles with same number --> check if both identical, else: error
+
+                    # Ednuerf Fneuf-1-Pittis AVCHD1080p fuer DVD5.Ger.part025.rar&quot; (27/99) yEnc (1/136)
+                    # <segment bytes="398458" number="27">1Ph1PL9Er40A8WQkste2_27o99@JBinUp.local</segment>
+                    # <segment bytes="398380" number="27">QrivyXCU7lV26U7sqtW9_27o99@JBinUp.local</segment>
+                    # '<1Ph1PL9Er40A8WQkste2_27o99@JBinUp.local>'
+                    # '<QrivyXCU7lV26U7sqtW9_27o99@JBinUp.local>'
+
+                    '''cat '<1Ph1PL9Er40A8WQkste2_27o99@JBinUp.local>'
+                    =ybegin part=27 total=134 line=128 size=51200000 name=Ednuerf Fneuf-1-Pittis AVCHD1080p fuer DVD5.Ger.part025.rar
+                    =ypart begin=9984001 end=10368000
+                    =yend size=384000 part=27 pcrc32=7d2799b9
+                    check crc32: 7d2799b9
+                    check length: 384000'''
+
+                    '''cat '<QrivyXCU7lV26U7sqtW9_27o99@JBinUp.local>'
+                    =ybegin part=27 total=134 line=128 size=51200000 name=Ednuerf Fneuf-1-Pittis AVCHD1080p fuer DVD5.Ger.part025.rar
+                    =ypart begin=9984001 end=10368000
+                    =yend size=384000 part=27 pcrc32=7d2799b9
+                    check crc32: 7d2799b9
+                    check length: 384000'''
+
+                    '''byt1 = bytearray()
+                    with open(COMPLETE_DIR + "articles/" + f[0], "w") as ff1:
+                        for inf in info_h.lines:
+                            try:
+                                inf0 = inf.decode()
+                                ff1.write(inf0 + "\n")
+                            except:
+                                byt1.extend(inf)
+                        length, crc32, decoded = yenc.decode(byt1)
+                        ff1.write("check crc32: " + str(crc32) + "\n")
+                        ff1.write("check length: " + str(length) + "\n")
+                        ff1.close()'''
+
                     headerfound = 0
                     endfound = 0
                     partfound = 0
@@ -105,21 +133,12 @@ class Download(Thread):
                         except Exception as e:
                             pass
                         byt.extend(inf)
-                    #print(info.lines[0])
-                    #print(info.lines[1])
-                    #print(info.lines[2])
-                    #print(info.lines[3])
-                    #print(headerfound, endfound)
                     if headerfound != 1 or endfound != 1 or partfound > 1:
                         byt = byt0
                         continue
                     articlesprocessed.append(f[1])
-                    # del info.lines[-1]
-                    # del info.lines[0:2]
-                    #for inf in info.lines:
-                    #    byt.extend(inf)
             length, crc32, decoded = yenc.decode(byt)
-            f0 = open("download/" + header, "wb")
+            f0 = open(COMPLETE_DIR + header, "wb")
             f0.write(decoded)
             print(header + " saved!!")
             f0.flush()
@@ -129,17 +148,34 @@ class Download(Thread):
 
 if __name__ == "__main__":
 
-    nzbbussicfg = configparser.ConfigParser()
-    nzbbussicfg.read("config/nzbbussi.config")
-    SERVER = nzbbussicfg["SERVERS"]["SERVER"]
-    USER = nzbbussicfg["SERVERS"]["USER"]
-    PASSWORD = nzbbussicfg["SERVERS"]["PASSWORD"]
-    PORT = int(nzbbussicfg["SERVERS"]["PORT"])
+    CFG = configparser.ConfigParser()
+    CFG.read(CONFIG_DIR + "/nzbbussi.config")
 
-    tree = ET.parse(NZB)
+    # only 1 server atm hardcoded
+    SERVER_NAME = CFG["SERVER1"]["SERVER_NAME"]
+    SERVER = CFG["SERVER1"]["SERVER_URL"]
+    USER = CFG["SERVER1"]["USER"]
+    PASSWORD = CFG["SERVER1"]["PASSWORD"]
+    PORT = int(CFG["SERVER1"]["PORT"])
+    SSL = True if CFG["SERVER1"]["SSL"].lower() == "yes" else False
+    LEVEL = int(CFG["SERVER1"]["LEVEL"])
+    CONNECTIONS = int(CFG["SERVER1"]["CONNECTIONS"])
+
+    # get nzbs -> atm only pls only 1 nzb in nzb dir!
+    os.chdir(NZB_DIR)
+    print("Getting NZB files from " + NZB_DIR)
+    for NZB in glob.glob("*.nzb"):
+        pass
+    try:
+        tree = ET.parse(NZB)
+        print("Downloading " + NZB)
+    except Exception as e:
+        print(str(e) + ": please provide at least 1 NZB file, exiting!")
+        sys.exit()
     root = tree.getroot()
+    os.chdir(MAIN_DIR)
 
-    connections = 10
+    connections = CONNECTIONS
 
     server = nntplib.NNTP_SSL(SERVER, user=USER, password=PASSWORD, port=PORT, readermode=True)
 
@@ -182,8 +218,8 @@ if __name__ == "__main__":
                 # print(filelist)
                 #print("-" * 80)
                 filedic[hn] = filelist
-    
-    '''threadlist = []
+
+    threadlist = []
     for co in range(connections):
         threadlist.append([])
     sn = 0
@@ -205,12 +241,12 @@ if __name__ == "__main__":
         th.start()
 
     for th in threads:
-        th.join()'''
+        th.join()
 
     print("par repair:")
 
     # PAR-REPAIR
-    os.chdir("download/")
+    os.chdir(COMPLETE_DIR)
     ssh = subprocess.Popen(["/usr/bin/par2verify", PAR2FILE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # ssh = subprocess.Popen(["ls", "-al"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     sshres = ssh.stdout.readlines()
@@ -219,6 +255,7 @@ if __name__ == "__main__":
         print("No par repair required")
     else:
         print("par2 repair required")
+        # TO DO: only download all par2 files if we are here
         ssh = subprocess.Popen(["/usr/bin/par2repair", PAR2FILE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         sshres = ssh.stdout.readlines()
         answer = sshres[-1].decode("utf-8")
@@ -226,4 +263,7 @@ if __name__ == "__main__":
             print("Repair complete!")
         else:
             print("Cannot repair!")
+    os.chdir(MAIN_DIR)
     # UNRAR
+
+    # DELETE ALL INTERIM FILES
