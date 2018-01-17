65975567388a78559b4213ed39f4922dd1444cad
dermatty <stephan@untergrabner.at>
2018-01-17 21:37:04 +0100

    article download refinement

--- nzbbussi.py
+++ nzbbussi.py
@@ -28,6 +28,10 @@ LOGS_DIR = MAIN_DIR + "logs/"
 SEMAPHORE = posix_ipc.Semaphore("news_sema", posix_ipc.O_CREAT)
 SEMAPHORE.release()
 
+FAILED_ARTICLE_LIST = []
+# 0 = started, 1 = Full Success, 2 = Articles missing, -1 = finally no success -> repair useless, too many articles missing
+ACTIVE_THREADS_STATUS = []
+
 BYTES_WRITTEN = 0
 DTIME = 0
 AVGBYTES = 0
@@ -46,6 +50,20 @@ def writer(nobyt):
     SEMAPHORE.release()
 
 
+def set_global_active_thread_status(nr, status):
+    global ACTIVE_THREADS_STATUS
+    SEMAPHORE.acquire()
+    ACTIVE_THREADS_STATUS[nr] = (status)
+    SEMAPHORE.release()
+
+
+def append_global_failed_articels(failedarticles):
+    global FAILED_ARTICLE_LIST
+    SEMAPHORE.acquire()
+    FAILED_ARTICLE_LIST.append(failedarticles)
+    SEMAPHORE.release()
+
+
 class Download(Thread):
 
     def __init__(self, threadno, items):
@@ -59,14 +77,67 @@ class Download(Thread):
         self.THREADNO = threadno
         self.SERVER = nntplib.NNTP_SSL(SERVER, user=USER, password=PASSWORD, ssl_context=self.context, port=PORT, readermode=True)
 
+    # downloads an article, returns list of data
+    def download_article(self, articles):
+        infolist = []
+        for art in articles:
+            print("Downloading article #" + str(art[1]) + ": " + art[0])
+            try:
+                resp_h, info_h = self.SERVER.head(art[0])  # 221 = article retrieved head follows: 222 0 <rocWBjTgD4RpOrSMspot_6o99@JBinUp.local>
+                resp, info = self.SERVER.body(art[0])      # 222 = article retrieved head follows: 221 0 <TDG36IMQlKcVW5NHovr11_10o99@JBinUp.local>
+                if resp_h[:3] != "221" or resp[:3] != "222":
+                    continue
+            except Exception as e:
+                print("Article download error: " + str(e))
+                continue
+            infolist.append(info)
+            break
+        return infolist
+
+    # yenc decode infolist from article(s)
+    def decode_articles(self, infolist):
+        headerfound = 0
+        endfound = 0
+        partfound = 0
+        byt = bytearray()
+        pcrc32 = ""
+        for info in infolist:
+            for inf in info.lines:
+                try:
+                    inf0 = inf.decode()
+                    if inf0 == "":
+                            continue
+                    if inf0[:7] == "=ybegin":
+                        headerfound += 1
+                        continue
+                    if inf0[:5] == "=yend":
+                        pcrc32 = inf0.split("pcrc32=")[1]
+                        endfound += 1
+                        continue
+                    if inf0[:6] == "=ypart":
+                        partfound += 1
+                        continue
+                except Exception as e:
+                    pass
+                byt.extend(inf)
+            if headerfound != 1 or endfound != 1 or partfound > 1:
+                print("Wrong yenc structure detected")
+                byt.clear()
+                continue           # try next article if provided
+            else:
+                break              # all fine, return first article as bytearray + crc32
+        return byt, pcrc32
+
     def run(self):
         global BYTES_WRITTEN, COMPLETE_DIR
         failedarticles = []
         # loop over whole (.rar) file
+        success = False
         for header, articlelist_ext in self.ITEMS:
             nr_articles = int(articlelist_ext[0])
             articlelist = articlelist_ext[1:]
             byt = bytearray()
+            # downlaod all articles
             for artnr in range(nr_articles):
                 articles_for_download = [f for f in articlelist if f[1] == artnr + 1]
                 # if article no. not found in articlelist -> failedarticles
@@ -74,153 +145,46 @@ class Download(Thread):
                     print("Article #" + str(artnr+1) + " is missing in NZB file")
                     failedarticles.append((header, artnr + 1, nr_articles))
                     continue
-                infolist = []
-                for a0 in articles_for_download:
-                    print(str(self.THREADNO) + " > File:" + header + " --> downloading article #" + str(a0[1]) + " of " + str(nr_articles) + ": " + a0[0])
-                    try:
-                        resp_h, info_h = self.SERVER.head(a0[0])  # 221 = article retrieved head follows: 222 0 <rocWBjTgD4RpOrSMspot_6o99@JBinUp.local>
-                        resp, info = self.SERVER.body(a0[0])      # 222 = article retrieved head follows: 221 0 <TDG36IMQlKcVW5NHovr11_10o99@JBinUp.local>
-                        if resp_h[:3] != "221" or resp[:3] != "222":
-                            continue
-                    except Exception as e:
-                        print("Article download error: " + str(e))
-                        continue
-                    infolist.append(info)
-                    break
+                infolist = self.download_article(articles_for_download)
                 # if cannot download article -> failedarticles
                 if infolist == []:
                     print("Download of Article failed")
                     failedarticles.append((header, artnr + 1, nr_articles))
                     continue
                 # all ok until now, decode it!
-                headerfound = 0
-                endfound = 0
-                partfound = 0
-                byt0 = byt
-                info = infolist[0]            # TODO: if duplicates found, iterate over them
-                for inf in info.lines:
-                    try:
-                        inf0 = inf.decode()
-                        if inf0 == "":
-                            continue
-                        if inf0[:7] == "=ybegin":
-                            headerfound += 1
-                            continue
-                        if inf0[:5] == "=yend":
-                            pcrc32 = inf0.split("pcrc32=")[1]
-                            endfound += 1
-                            continue
-                        if inf0[:6] == "=ypart":
-                            partfound += 1
-                            continue
-                    except Exception as e:
-                        pass
-                    byt.extend(inf)
-                if headerfound != 1 or endfound != 1 or partfound > 1:
-                    print("Wrong yenc structure detected")
-                    byt = byt0
+                article_bytes, pcrc32 = self.decode_articles(infolist)
+                if pcrc32 == "":
+                    failedarticles.append((header, artnr + 1, nr_articles))
+                    print("Could not decode article!")
                     continue
-                # print("Article passed successfully")
-            # print("------------------------------------")
-            length, crc32, decoded = yenc.decode(byt)
-            print(crc32, pcrc32)
-            if crc32 == pcrc32:
-                # print("CRC check success!")
-                f0 = open(COMPLETE_DIR + header, "wb")
-                f0.write(decoded)
+                # check crc32
+                _, crc32, decoded = yenc.decode(article_bytes)
+                if crc32.strip("0") != pcrc32.strip("0"):
+                    failedarticles.append((header, artnr + 1, nr_articles))
+                    print("CRC32 checksum error: " + crc32 + " / " + pcrc32)
+                    continue
+                # all ok, add bytes from article to file
+                byt.extend(decoded)
+            with open(COMPLETE_DIR + header, "wb") as f0:
+                f0.write(byt)
                 f0.flush()
                 f0.close()
-            else:
-                print("CRC32 failed")
+                print(header + " saved!!")
+                success = True
+        append_global_failed_articels(failedarticles)
+        if success:
+            set_global_active_thread_status(self.THREADNO, 1)
+        else:
+            set_global_active_thread_status(self.THREADNO, 2)
         self.SERVER.quit()
 
-        '''for header, filelist in self.ITEMS:
-            nr_articles = int(filelist[0])
-            articlesprocessed = []
-            byt = bytearray()
-            for f in filelist:
-                if f == filelist[0]:
-                    continue
-                if f[1] not in articlesprocessed:
-                    print(str(self.THREADNO) + " > File:" + header + " --> downloading article #" + str(f[1]) + " of " + str(nr_articles) + ": " + f[0])
-                    resp_h, info_h = self.SERVER.head(f[0])  # 221 = article retrieved head follows: 222 0 <rocWBjTgD4RpOrSMspot_6o99@JBinUp.local>
-                    resp, info = self.SERVER.body(f[0])      # 222 = article retrieved head follows: 221 0 <TDG36IMQlKcVW5NHovr11_10o99@JBinUp.local>
-                    print(resp_h[:3])
-                    sys.exit()
-                    # todo: if 2 articles with same number --> check:
-                    #    a. both size & crc32 are ok
-                    #    b. if both are ok and identical crc -> take first one
-                    #    c. if only 1 is ok -> take this
-                    #    d. if both are ok but not identical crc/size --> error
-                    #    e. if article is missing try other server, oder so
-
-                    # Ednuerf Fneuf-1-Pittis AVCHD1080p fuer DVD5.Ger.part025.rar&quot; (27/99) yEnc (1/136)
-                    # <segment bytes="398458" number="27">1Ph1PL9Er40A8WQkste2_27o99@JBinUp.local</segment>
-                    # <segment bytes="398380" number="27">QrivyXCU7lV26U7sqtW9_27o99@JBinUp.local</segment>
-                    # '<1Ph1PL9Er40A8WQkste2_27o99@JBinUp.local>'
-                    # '<QrivyXCU7lV26U7sqtW9_27o99@JBinUp.local>'
-
-                    cat '<1Ph1PL9Er40A8WQkste2_27o99@JBinUp.local>'
-                    =ybegin part=27 total=134 line=128 size=51200000 name=Ednuerf Fneuf-1-Pittis AVCHD1080p fuer DVD5.Ger.part025.rar
-                    =ypart begin=9984001 end=10368000
-                    =yend size=384000 part=27 pcrc32=7d2799b9
-                    check crc32: 7d2799b9
-                    check length: 384000
-
-                    cat '<QrivyXCU7lV26U7sqtW9_27o99@JBinUp.local>'
-                    =ybegin part=27 total=134 line=128 size=51200000 name=Ednuerf Fneuf-1-Pittis AVCHD1080p fuer DVD5.Ger.part025.rar
-                    =ypart begin=9984001 end=10368000
-                    =yend size=384000 part=27 pcrc32=7d2799b9
-                    check crc32: 7d2799b9
-                    check length: 384000
-
-                    byt1 = bytearray()
-                    with open(COMPLETE_DIR + "articles/" + f[0], "w") as ff1:
-                        for inf in info_h.lines:
-                            try:
-                                inf0 = inf.decode()
-                                ff1.write(inf0 + "\n")
-                            except:
-                                byt1.extend(inf)
-                        length, crc32, decoded = yenc.decode(byt1)
-                        ff1.write("check crc32: " + str(crc32) + "\n")
-                        ff1.write("check length: " + str(length) + "\n")
-                        ff1.close()
-
-                    headerfound = 0
-                    endfound = 0
-                    partfound = 0
-                    # check article integrity
-                    # todo: check more thoroughly!! see:
-                    #          http://www.yenc.org/yenc-draft.1.3.txt
-                    byt0 = byt
-                    for inf in info.lines:
-                        try:
-                            inf0 = inf.decode()
-                            if inf0 == "":
-                                continue
-                            if inf0[:7] == "=ybegin":
-                                headerfound += 1
-                                continue
-                            if inf0[:5] == "=yend":
-                                endfound += 1
-                                continue
-                            if inf0[:6] == "=ypart":
-                                partfound += 1
-                                continue
-                        except Exception as e:
-                            pass
-                        byt.extend(inf)
-                    if headerfound != 1 or endfound != 1 or partfound > 1:
-                        byt = byt0
-                        continue
-                    articlesprocessed.append(f[1])
-            length, crc32, decoded = yenc.decode(byt)
-            f0 = open(COMPLETE_DIR + header, "wb")
-            f0.write(decoded)
-            print(header + " saved!!")
-            f0.flush()
-            f0.close()'''
+        '''
+         =ybegin part=27 total=134 line=128 size=51200000 name=Ednuerf Fneuf-1-Pittis AVCHD1080p fuer DVD5.Ger.part025.rar
+         =ypart begin=9984001 end=10368000
+         =yend size=384000 part=27 pcrc32=7d2799b9
+         check crc32: 7d2799b9
+         check length: 384000
+        '''
 
 
 if __name__ == "__main__":
@@ -252,7 +216,7 @@ if __name__ == "__main__":
     root = tree.getroot()
     os.chdir(MAIN_DIR)
 
-    connections = 1  # CONNECTIONS
+    connections = CONNECTIONS
 
     server = nntplib.NNTP_SSL(SERVER, user=USER, password=PASSWORD, port=PORT, readermode=True)
 
@@ -314,11 +278,40 @@ if __name__ == "__main__":
         sn += 1
 
     for th in threads:
+        ACTIVE_THREADS_STATUS.append(0)
         print("Starting Thread")
         th.start()
 
+    # 1 = full success
+    # -1 = par2 repair makes sense
+    # -2 = par2 repair makes NO sense
+    downloadstatus = 0
+    while True:
+        # Download ended with "Article missing" -> try to get from fill server, if any
+        # if no fill server -> check if too much articels are missing
+        # 0 = started
+        # 1 = Full Success
+        # 2 = Articles missing, start thread for alternative download or check if par2 makes sense
+        # -1 = cannot redownload missing articles, but par2 repair makes sense
+        # -2 = cannot redownload missing articles and par2 repair makes NO sense
+        if 2 in ACTIVE_THREADS_STATUS:
+            # try to redownload, from other server. if no server left, check if repair is useless
+            pass
+        # if status for all threads is only "success" or "no success": exit loop
+        if 0 not in ACTIVE_THREADS_STATUS and 2 not in ACTIVE_THREADS_STATUS:
+            if -1 not in ACTIVE_THREADS_STATUS and -2 not in ACTIVE_THREADS_STATUS:
+                downloadstatus = 1
+            break
+        time.sleep(1)
+
+    sys.exit()
+        
     for th in threads:
+        print("------------------------")
         th.join()
+
+
+
 
     print("par repair:")
 
