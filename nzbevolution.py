ac04d6aafa4d74625f695cf840868dfe3d531313
dermatty <stephan@untergrabner.at>
2018-01-31 07:55:00 +0100

    producer not in a thread!

--- nzbevolution.py
+++ nzbevolution.py
@@ -21,7 +21,6 @@ def download(article, nntpob):
 class ConnectionWorker(Thread):
     def __init__(self, lock, connection, articlequeue):
         Thread.__init__(self)
-        self.daemon = True
         self.connection = connection
         self.articlequeue = articlequeue
         self.lock = lock
@@ -39,28 +38,30 @@ class ConnectionWorker(Thread):
             # storedic[idn] = articlequeue.get()  # articlenr, remaining_servers = article
             try:
                 storedic[idn] = self.articlequeue.get_nowait()  # articlenr, remaining_servers = article
+                self.articlequeue.task_done()
             except Exception as e:
                 continue
             if name not in storedic[idn][1]:
-                self.articlequeue.task_done()            # ????????
+                # self.articlequeue.task_done()            # ????????
                 self.articlequeue.put(storedic[idn])
             else:
                 print("Downloading on server " + idn + ": + for article #" + str(storedic[idn][0]), storedic[idn][1])
                 res, info = download(storedic[idn], nntpobj)
+                # self.articlequeue.task_done()
                 if res:
                     print("Download success on server " + idn + ": for article #" + str(storedic[idn][0]), storedic[idn][1])
                     with self.lock:
                         resultarticles_dic[str(storedic[idn][0])] = info     # mit semaphore
-                    self.articlequeue.task_done()
+                    # self.articlequeue.task_done()
                 else:
                     storedic[idn] = (storedic[idn][0], [x for x in storedic[idn][1] if x != name])
                     if not storedic[idn][1]:
                         print(">>>> Download finally failed on server " + idn + ": for article #" + str(storedic[idn][0]), storedic[idn][1])
                         with self.lock:
                             resultarticles_dic[str(storedic[idn][0])] = None     # mit semaphore
-                        self.articlequeue.task_done()
+                        # self.articlequeue.task_done()
                     else:
-                        self.articlequeue.task_done()    # ?????????????
+                        # self.articlequeue.task_done()    # ?????????????
                         self.articlequeue.put(storedic[idn])
                         print(">>>> Download failed on server " + idn + ": for article #" + str(storedic[idn][0]), ", requeuing on servers:",
                               storedic[idn][1])
@@ -68,18 +69,14 @@ class ConnectionWorker(Thread):
         print(idn + " exited!")
 
 
-# producer
-def articleproducer(articles):
-    # the main thread will put new articles to the queue
+def ArticleProducer(articles, articlequeue):
     for article in articles:
-        print(".", end="")
         articlequeue.put(article)
-    # articlequeue.join()
 
 
 # main
 if __name__ == '__main__':
-    nr_articles = 500
+    nr_articles = 134
     storedic = {}
     articlequeue = queue.Queue()
     articles0 = [str(i+1) for i in range(nr_articles)]
@@ -93,6 +90,7 @@ if __name__ == '__main__':
 
     level_servers0 = {"0": ["EWEKA", "BULK"], "1": ["TWEAK"], "2": ["NEWS", "BALD"]}
 
+    t0 = time.time()
     for level, serverlist in level_servers0.items():
         level_servers = serverlist
         articles = [(key, level_servers) for key, item in resultarticles_dic.items() if not item]
@@ -103,31 +101,20 @@ if __name__ == '__main__':
         level_connections = [(name, connection) for name, connection in all_connections if name in level_servers]
         if not level_connections:
             continue
+        # Produce
+        ArticleProducer(articles, articlequeue)
         # consumer
         threads = []
         for c in level_connections:
-            # t = threading.Thread(target=connectionworker, args=(c, articlequeue))
             t = ConnectionWorker(LOCK, c, articlequeue)
             threads.append(t)
             t.start()
 
-        # Produce
-        t = threading.Thread(target=articleproducer, args=(articles, ))
-        t.start()
         articlequeue.join()
-
-        #while articlequeue.qsize() > 0:
-        #    pass
 
         for t in threads:
             t.stop()
-            while t.isAlive():
-                time.sleep(0.05)
-                pass
-
-
-
-        # input()
+            t.join()
 
         print("Download failed:", [(key, item) for key, item in resultarticles_dic.items() if not item])
         l0 = len([item for key, item in resultarticles_dic.items()])
@@ -136,3 +123,4 @@ if __name__ == '__main__':
         print("Complete  Articles after level", level, ": " + str(l1) + " out of " + str(l0))
         # print(resultarticles_dic)
         print("-" * 80)
+    print(time.time() - t0)
