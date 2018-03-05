48d62eacbd83da4a63d3e0766030f478e681eb6a
dermatty <stephan@untergrabner.at>
2018-03-05 22:34:42 +0100

    license in par2 analyzer

--- par2.py
+++ par2.py
@@ -4,9 +4,34 @@
 # All credits go to:
 # https://github.com/jmoiron/par2ools/blob/master/par2ools/par2.py
 
+'''
+Copyright (c) 2010 Jason Moiron and Contributors
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
+
 """A native python implementation of the par2 file format.
 This is only intended to be able to read packets in par2, not repair,
 verify, or create new par2 files."""
+
 
 # import sys
 import fnmatch
