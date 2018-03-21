e7c59740367d5fb876880ff833a7695bfd5a417e
dermatty <stephan@untergrabner.at>
2018-03-21 21:13:39 +0100

    renamer working

--- par2lib.py
+++ par2lib.py
@@ -588,9 +588,8 @@ def renamer(source_dir, dest_dir, logger):
             if fn0.endswith(".par2") and not p2obj:
                 p2obj = Par2File(fn)
                 p2basename = fn.split(".par2")[0]
+                # todo: test par2
             renamedfiles.append(fn0)
-        print(renamedfiles)
-        print("-" * 50)
 
         # if par2 not in _renamed search main par2 in _downloaded0
         if not p2obj:
@@ -603,13 +602,14 @@ def renamer(source_dir, dest_dir, logger):
                     mainparlist.append((fn, filelen))
             if mainparlist:
                 p2min = sorted(mainparlist, key=lambda length: length[1])[0][0]
+                # todo: teste par2 file!!!
                 p2obj = Par2File(p2min)
-                p2basename = p2min
+                p2basename = p2min.split(".par2")[0]
                 print("p2obj found: " + str(p2min))
 
         # search for not yet renamed par2/vol files
         not_renamed_par2list = []
-        for pname, _ in notrenamedfiles:
+        for pname, phash in notrenamedfiles:
             with open(pname, "rb") as f:
                 content = f.read()
                 # check if PAR 2.0\0Creator\0 in last 50 bytes
@@ -621,33 +621,40 @@ def renamer(source_dir, dest_dir, logger):
                 # check for PAR2\x00 in first 50 bytes
                 # if no -> it's par2vol, else: could be par2
                 firstcontent = content[:100]
-                bstr0 = b"PAR2\0"
-                bstr1 = b"PAR 2.0\0FileDesc"
+                bstr0 = b"PAR2\x00"  # PAR2\x00
+                bstr1 = b"PAR 2.0\x00FileDesc"  # PAR 2.0\x00FileDesc'
                 if bstr0 not in firstcontent:
                     ptype = "par2vol"
                 elif bstr1 in firstcontent:
                     ptype = "par2"
+                    p2obj = Par2File(pname)
+                    p2basename = pname.split(".par2")[0]
                 else:
                     ptype = "par2vol"
-                not_renamed_par2list.append((pname, ptype))
+                not_renamed_par2list.append((pname, ptype, phash))
+        # print(not_renamed_par2list)
         if not_renamed_par2list:
-            for pname, ptype in not_renamed_par2list:
+            for pname, ptype, phash in not_renamed_par2list:
+                pp = (pname, phash)
                 if ptype == "par2":
-                    p2obj = Par2File(fn)
-                    p2basename = fn.split(".par2")[0]
                     shutil.copyfile(source_dir + pname, dest_dir + pname)
                     os.rename(source_dir + pname, source_dir + pname + ".renamed")
+                    notrenamedfiles.remove(pp)
                 elif ptype == "par2vol" and p2basename:
+                    # todo: if not p2basename ??
                     volpart1 = randint(1, 99)
                     volpart2 = randint(1, 99)
                     shutil.copyfile(source_dir + pname, dest_dir + p2basename + ".vol" + str(volpart1).zfill(3) +
                                     "+" + str(volpart2).zfill(3) + ".PAR2")
                     os.rename(source_dir + pname, source_dir + pname + ".renamed")
+                    notrenamedfiles.remove(pp)
 
         # rename + move rar files
         if p2obj:
             rarfileslist = p2obj.md5_16khash()
-            for a_name, a_md5 in notrenamedfiles:
+            notrenamedfiles0 = notrenamedfiles[:]
+            for a_name, a_md5 in notrenamedfiles0:
+                pp = (a_name, a_md5)
                 try:
                     r_name = [fn for fn, r_md5 in rarfileslist if r_md5 == a_md5][0]
                     if r_name != a_name:
@@ -655,6 +662,7 @@ def renamer(source_dir, dest_dir, logger):
                     else:
                         shutil.copyfile(source_dir + a_name, dest_dir + a_name)
                     os.rename(source_dir + a_name, source_dir + a_name + ".renamed")
+                    notrenamedfiles.remove(pp)
                 except IndexError:
                     pass
                 except Exception as e:
@@ -664,7 +672,10 @@ def renamer(source_dir, dest_dir, logger):
         # .
         # .
         # .
-
+        for a_name, a_md5 in notrenamedfiles:
+            shutil.copyfile(source_dir + a_name, dest_dir + a_name)
+            os.rename(source_dir + a_name, source_dir + a_name + ".renamed")
+        time.sleep(2)
     os.chdir(cwd0)
 
 
