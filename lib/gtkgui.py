f44bd21a33b6364a90c108c4f88a62dab61bc7a5
dermatty <stephan@untergrabner.at>
2018-06-07 07:40:39 +0200

    list & scrolled window

--- lib/gtkgui.py
+++ lib/gtkgui.py
@@ -1,13 +1,14 @@
 import sys
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gio, Gdk, GdkPixbuf
+from gi.repository import Gtk, Gio, Gdk, GdkPixbuf, GLib
 
 
 __appname__ = "Ginzibix"
 __version__ = "0.01 pre-alpha"
 __author__ = "dermatty"
 
+GBXICON = "ginzibix.jpe"
 
 MENU_XML = """
 <?xml version="1.0" encoding="UTF-8"?>
@@ -32,20 +33,26 @@ MENU_XML = """
 class AppData:
     def __init__(self):
         self.mbitsec = 0
-        self.appname = __appname__
+        # NZB - progress - downloaded - overall - eta
+        self.nzbs = [("Fuenf Freunde", 0, 1.21, 2.67, "11m03s"),
+                     ("Der Gloeckner von Notredame", 0, 0.0, 0.98, "7m14s")]
+        for i in range(122):
+            self.nzbs.append(self.nzbs[0])
 
 
 class AppWindow(Gtk.ApplicationWindow):
 
     def __init__(self, app):
         self.appdata = AppData()
-        Gtk.Window.__init__(self, title=self.appdata.appname, application=app)
-        self.set_icon_from_file("index.jpe")
-
+        Gtk.Window.__init__(self, title=__appname__, application=app)
+        try:
+            self.set_icon_from_file(GBXICON)
+        except GLib.GError as e:
+            print("Cannot find icon file!")
         # init main window
         self.set_border_width(10)
         # self.set_default_size(600, 200)
-        self.set_wmclass(self.appdata.appname, self.appdata.appname)
+        self.set_wmclass(__appname__, __appname__)
         self.header_bar()
         box_main = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(box_main)
@@ -55,7 +62,7 @@ class AppWindow(Gtk.ApplicationWindow):
         stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
         stack.set_transition_duration(200)
 
-        self.stacknzb_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=32)
+        self.stacknzb_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=12)
         stack.add_titled(self.stacknzb_box, "nzbs", "NZBs")
         self.show_nzb_stack(self.stacknzb_box)
 
@@ -68,22 +75,73 @@ class AppWindow(Gtk.ApplicationWindow):
         stack_switcher = Gtk.StackSwitcher()
         stack_switcher.set_stack(stack)
         stack_switcher.set_property("halign", Gtk.Align.CENTER)
-        stack_switcher.set_property("valign", Gtk.Align.CENTER)
-        box_main.pack_start(stack_switcher, True, True, 0)
+        stack_switcher.set_property("valign", Gtk.Align.START)
+        box_main.pack_start(stack_switcher, False, False, 0)
         box_main.pack_start(stack, True, True, 0)
-        
+
     def show_nzb_stack(self, stacknzb_box):
-        buttonbox = Gtk.Box(spacing=26)
-        buttonbox.set_property("halign", Gtk.Align.START)
-        buttonbox.set_property("valign", Gtk.Align.CENTER)
-        stacknzb_box.pack_start(buttonbox, True, True, 0)
-        button1 = Gtk.Button.new_from_icon_name("media-playback-stop", Gtk.IconSize.BUTTON)
-        buttonbox.pack_start(button1, True, True, 0)
+        # scrolled window
+        scrolled_window = Gtk.ScrolledWindow()
+        scrolled_window.set_border_width(10)
+        # there is always the scrollbar (otherwise: AUTOMATIC - only if needed
+        # - or NEVER)
+        scrolled_window.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scrolled_window.set_property("min-content-height", 300)
+        # scrolled_window.set_property("valign", Gtk.Align.START)
+        stacknzb_box.pack_start(scrolled_window, True, True, 16)
+
+        # listbox
+        listbox = Gtk.ListBox()
+        
+        # stacknzb_box.pack_start(listbox, True, True, 16)
+
+        row = Gtk.ListBoxRow()
+
+        self.liststore = Gtk.ListStore(str, int, float, float, str)
+        for i, nzb in enumerate(self.appdata.nzbs):
+            if i == 0:
+                self.current_iter = self.liststore.append(list(nzb))
+            else:
+                self.liststore.append(list(nzb))
+
+        treeview = Gtk.TreeView(model=self.liststore)
+        renderer_text0 = Gtk.CellRendererText()
+        column_text0 = Gtk.TreeViewColumn("NZB name", renderer_text0, text=0)
+        column_text0.set_expand(True)
+        treeview.append_column(column_text0)
+
+        renderer_progress = Gtk.CellRendererProgress()
+        column_progress = Gtk.TreeViewColumn("Progress", renderer_progress, value=1)
+        column_progress.set_min_width(260)
+        treeview.append_column(column_progress)
+
+        renderer_text1 = Gtk.CellRendererText()
+        column_text1 = Gtk.TreeViewColumn("Downloaded", renderer_text1, text=2)
+        column_text1.set_cell_data_func(renderer_text1, lambda col, cell, model, iter, unused:
+                                        cell.set_property("text", "{0:.2f}".format(model.get(iter, 2)[0]) + " GiB"))
+        treeview.append_column(column_text1)
+
+        renderer_text2 = Gtk.CellRendererText()
+        column_text2 = Gtk.TreeViewColumn("Overall", renderer_text2, text=3)
+        column_text2.set_cell_data_func(renderer_text2, lambda col, cell, model, iter, unused:
+                                        cell.set_property("text", "{0:.2f}".format(model.get(iter, 3)[0]) + " GiB"))
+        column_text2.set_min_width(80)
+        treeview.append_column(column_text2)
+
+        renderer_text3 = Gtk.CellRendererText()
+        column_text3 = Gtk.TreeViewColumn("Eta", renderer_text3, text=4)
+        column_text3.set_min_width(80)
+        treeview.append_column(column_text3)
+
+        row.add(treeview)
+        listbox.add(row)
+        scrolled_window.add(listbox)
+
 
     def header_bar(self):
         hb = Gtk.HeaderBar(spacing=20)
         hb.set_show_close_button(True)
-        hb.props.title = self.appdata.appname
+        hb.props.title = __appname__
         self.set_titlebar(hb)
 
         button = Gtk.Button()
@@ -122,11 +180,14 @@ class Application(Gtk.Application):
         about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
         about_dialog.set_program_name(__appname__)
         about_dialog.set_version(__version__)
-        about_dialog.set_copyright("(c) dermatty 2018")
+        about_dialog.set_copyright("Copyright \xa9 2018 dermatty")
         about_dialog.set_comments("A binary newsreader for the gnome desktop")
         about_dialog.set_website("https://github.com/dermatty/GINZIBIX")
-        about_dialog.set_website_label('https://github.com/dermatty/GINZIBIX')
-        about_dialog.set_logo(GdkPixbuf.Pixbuf.new_from_file_at_size("index.jpe", 64, 64))
+        about_dialog.set_website_label('Ginzibix on GitHub')
+        try:
+            about_dialog.set_logo(GdkPixbuf.Pixbuf.new_from_file_at_size(GBXICON, 64, 64))
+        except GLib.GError as e:
+            print("Cannot find icon file!")
 
         about_dialog.set_license_type(Gtk.License.GPL_3_0)
 
