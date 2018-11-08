46a8470044b7db4ed22197e4f414b769750186f0
dermatty <stephan@untergrabner.at>
2018-11-08 21:09:01 +0100

    special if for ginzicut connect

--- lib/server.py
+++ lib/server.py
@@ -73,7 +73,11 @@ class Servers():
                             if usessl:
                                 nntpobj = nntplib.NNTP_SSL(server_url, user=user, password=password, ssl_context=context, port=port, readermode=True, timeout=5)
                             else:
-                                nntpobj = nntplib.NNTP(server_url, user=user, password=password, port=port, readermode=True, timeout=5)
+                                # this is just for ginzicut preloading
+                                if user.lower() == "ginzicut" or password.lower() == "ginzicut":
+                                    nntpobj = nntplib.NNTP(server_url, port=port)
+                                else:
+                                    nntpobj = nntplib.NNTP(server_url, user=user, password=password, readermode=True, port=port, timeout=5)
                             self.logger.debug(whoami() + "Opened Connection #" + str(conn_nr) + " on server " + server_name0)
                             result = nntpobj
                             self.all_connections[idx] = (sn, cn, rt, nntpobj)
